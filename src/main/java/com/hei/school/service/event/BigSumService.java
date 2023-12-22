614e2cbf5023e01cd49de99b00472344ae62c15b
Maharavo1 <132753016+Maharavo1@users.noreply.github.com>
2023-12-22 09:18:47 +0300

    feat : add BigSumController and BigSumService

---
+++ src/main/java/com/hei/school/service/event/BigSumService.java
@@ -0,0 +1,11 @@
+package com.hei.school.service.event;
+import java.math.BigInteger;
+import org.springframework.stereotype.Service;
+@Service
+public class BigSumService {
+    public BigInteger add(String a, String b) {
+        BigInteger bigsumA = new BigInteger(a);
+        BigInteger bigsumB = new BigInteger(b);
+        return bigsumA.add(bigsumB);
+    }
+}
